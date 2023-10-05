cc7a939bfafb31c9b4a5ba792e62df4525f28bad
Thnderbread <david.kiiing1227@gmail.com>
2023-10-05 05:25:05 -0400

    Updated environment variable

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
     try {
-        await mongoose.connect(process.env.DATABASE_URI, {
+        await mongoose.connect(process.env.MONGO_DATABASE_URI, {
             useUnifiedTopology: true,
             useNewUrlParser: true
         });
