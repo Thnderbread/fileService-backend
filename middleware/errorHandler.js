f297b0311d234f0b842fb62a1aebbcd24797f931
Thnderbread <david.kiiing1227@gmail.com>
2023-10-06 18:59:06 -0400

    Moved error logger into file with handleErrors function; deleted logErrors file.

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,7 +1,19 @@
-const logErrors = require("./logErrors");
+const { v4: uuid } = require('uuid');
+const createLogger = require("../logs/loggerConfig");
+const errorLogger = createLogger("file", "error", "../logs/errorLog.log");
 
 function handleErrors(error, req, res, next) {
-    logErrors(error, req, res)
+    errorLogger.error(`${uuid()} - 
+    Date: ${new Date().toISOString()} - 
+    Error: ${error.name}: ${error.message} - 
+    Method: ${req.method} -
+    Referrer: ${req.headers.referer} -
+    Original URI: ${req.originalUrl} - 
+    URI: ${req.url} -
+    Status: ${error.statusCode || res.statusCode}\n
+    Error Stack: ${error.stack}\n`
+    );
+
     res.header("Content-Type", "application/json")
 
     // these errors' messages can be shown safely.
