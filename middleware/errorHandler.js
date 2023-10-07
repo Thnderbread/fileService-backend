defa8035dd118b8c48fd5ef107f1b0580f838fcd
Thnderbread <david.kiiing1227@gmail.com>
2023-10-07 16:26:48 -0400

    Formatting & organization

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,29 +1,32 @@
 const { v4: uuid } = require('uuid');
-const createLogger = require("../logs/loggerConfig");
-const errorLogger = createLogger("file", "error", "../logs/errorLog.log");
+const createLogger = require("../helpers/loggerConfig");
+const errorLogger = createLogger("file", "error", "errorLog.log");
 
 function handleErrors(error, req, res, next) {
-    errorLogger.error(`${uuid()} - 
-    Date: ${new Date().toISOString()} - 
-    Error: ${error.name}: ${error.message} - 
-    Method: ${req.method} -
-    Referrer: ${req.headers.referer} -
-    Original URI: ${req.originalUrl} - 
-    URI: ${req.url} -
-    Status: ${error.statusCode || res.statusCode}\n
-    Error Stack: ${error.stack}\n`
+    errorLogger.error(
+        `Log id: ${uuid()}
+        Date: ${new Date().toISOString()}
+        Error: ${error.name}: ${error.message}
+        Method: ${req.method}
+        Referrer: ${req.headers.referer}
+        Original URI: ${req.originalUrl}
+        URI: ${req.url}
+        Response status code: ${error.statusCode || res.statusCode}
+        Error Stack: ${error.stack}\n`
     );
 
     res.header("Content-Type", "application/json")
 
     // these errors' messages can be shown safely.
-    const acceptableErrorCodes = [302, 400, 401, 409, 415, 507]
+    const acceptableStatusCodes = [302, 400, 401, 409, 415, 507]
 
     // in case of a 500-class error, or some other uncaught
     // exception thrown by something (i.e. third-party library)
     // use this method to avoid exposing sensible information.
-    if (!error || !acceptableErrorCodes.includes(error.statusCode)) {
-        return res
+    // statusCodes aren't set when an uncaught exception occurs as well.
+    if (!error || !error.statusCode ||
+        !acceptableStatusCodes.includes(error.statusCode)) {
+        res
             .status(500)
             .json({
                 success: false,
@@ -33,9 +36,9 @@ function handleErrors(error, req, res, next) {
             });
     }
 
-    return res
-        .status(error.statusCode)
-        .json({ error: error.displayMessage })
+    res.status(error.statusCode).json({ error: error.message })
+
+    next()
 }
 
 module.exports = handleErrors;
--- middleware/requestLogger.js
+++ middleware/requestLogger.js
@@ -1,16 +1,16 @@
 const { v4: uuid } = require('uuid');
-const createLogger = require('../logs/loggerConfig');
-const infoLogger = createLogger("file", "info", "../logs/infoLog.log");
+const createLogger = require('../helpers/loggerConfig');
+const infoLogger = createLogger("file", "info", "infoLog.log");
 
 function requestLogger(req, res, next) {
     res.on("finish", () => {
         infoLogger.info(
-            `${uuid()} - 
-            Date: ${new Date().toISOString()} - 
-            Method: ${req.method} - 
+            `Log id: ${uuid()}
+            Date: ${new Date().toISOString()}
+            Method: ${req.method}
             Referrer: ${req.headers.referer || ''}
-            Original URI: ${req.headers.origin} - 
-            URI: ${req.url} - 
+            Original URI: ${req.headers.origin}
+            URI: ${req.url}
             Status Code: ${res.statusCode}\n`
         );
     })
